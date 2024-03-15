5cee6cfc4b86e35bf86164cdc076098cacb0164a
Bernardo Fuentes <bfuentes@itivos.com>
2024-03-14 20:47:28 -0600

    error en icono

--- itivos_socialslink.php
+++ itivos_socialslink.php
@@ -7,14 +7,14 @@
 require_once(__DIR_MODULES__."itivos_socialslink/classes/itivos_socialslink.php");
 class ItivosSocialsLink extends modules
 {
-	public $html = "";
+    public $html = "";
     public function __construct()
-    {
+    { 
         $this->name ='itivos_socialslink';
         $this->displayName = "Social link";
         $this->description = $this->l('Agrega al footer enlaces de redes sociales');
         $this->category  ='front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'Bernardo Fuentes';
         $this->versions_compliancy = array('min'=>'1.0.0', 'max'=> __SYSTEM_VERSION__);
         $this->confirmUninstall = $this->l('Are you sure about removing these details?');
@@ -47,7 +47,7 @@ class ItivosSocialsLink extends modules
             array(
                 "name" => "youtube",
                 "link" => "https://youtube.com",
-                "icon" => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M194.4 211.7a53.3 53.3 0 1 0 59.3 88.7 53.3 53.3 0 1 0 -59.3-88.7zm142.3-68.4c-5.2-5.2-11.5-9.3-18.4-12c-18.1-7.1-57.6-6.8-83.1-6.5c-4.1 0-7.9 .1-11.2 .1c-3.3 0-7.2 0-11.4-.1c-25.5-.3-64.8-.7-82.9 6.5c-6.9 2.7-13.1 6.8-18.4 12s-9.3 11.5-12 18.4c-7.1 18.1-6.7 57.7-6.5 83.2c0 4.1 .1 7.9 .1 11.1s0 7-.1 11.1c-.2 25.5-.6 65.1 6.5 83.2c2.7 6.9 6.8 13.1 12 18.4s11.5 9.3 18.4 12c18.1 7.1 57.6 6.8 83.1 6.5c4.1 0 7.9-.1 11.2-.1c3.3 0 7.2 0 11.4 .1c25.5 .3 64.8 .7 82.9-6.5c6.9-2.7 13.1-6.8 18.4-12s9.3-11.5 12-18.4c7.2-18 6.8-57.4 6.5-83c0-4.2-.1-8.1-.1-11.4s0-7.1 .1-11.4c.3-25.5 .7-64.9-6.5-83l0 0c-2.7-6.9-6.8-13.1-12-18.4zm-67.1 44.5A82 82 0 1 1 178.4 324.2a82 82 0 1 1 91.1-136.4zm29.2-1.3c-3.1-2.1-5.6-5.1-7.1-8.6s-1.8-7.3-1.1-11.1s2.6-7.1 5.2-9.8s6.1-4.5 9.8-5.2s7.6-.4 11.1 1.1s6.5 3.9 8.6 7s3.2 6.8 3.2 10.6c0 2.5-.5 5-1.4 7.3s-2.4 4.4-4.1 6.2s-3.9 3.2-6.2 4.2s-4.8 1.5-7.3 1.5l0 0c-3.8 0-7.5-1.1-10.6-3.2zM448 96c0-35.3-28.7-64-64-64H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96zM357 389c-18.7 18.7-41.4 24.6-67 25.9c-26.4 1.5-105.6 1.5-132 0c-25.6-1.3-48.3-7.2-67-25.9s-24.6-41.4-25.8-67c-1.5-26.4-1.5-105.6 0-132c1.3-25.6 7.1-48.3 25.8-67s41.5-24.6 67-25.8c26.4-1.5 105.6-1.5 132 0c25.6 1.3 48.3 7.1 67 25.8s24.6 41.4 25.8 67c1.5 26.3 1.5 105.4 0 131.9c-1.3 25.6-7.1 48.3-25.8 67z"/></svg>'
+                "icon" => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M282 256.2l-95.2-54.1V310.3L282 256.2zM384 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64zm14.4 136.1c7.6 28.6 7.6 88.2 7.6 88.2s0 59.6-7.6 88.1c-4.2 15.8-16.5 27.7-32.2 31.9C337.9 384 224 384 224 384s-113.9 0-142.2-7.6c-15.7-4.2-28-16.1-32.2-31.9C42 315.9 42 256.3 42 256.3s0-59.7 7.6-88.2c4.2-15.8 16.5-28.2 32.2-32.4C110.1 128 224 128 224 128s113.9 0 142.2 7.7c15.7 4.2 28 16.6 32.2 32.4z"/></svg>'
             ),
             array(
                 "name" => "linkedin",
@@ -67,7 +67,7 @@ class ItivosSocialsLink extends modules
     }
     public function install()
     {
-    	 if(!$this->registerHook("displayHead") ||
+         if(!$this->registerHook("displayHead") ||
             !$this->registerHook("displayFrontHead") ||
             !$this->registerHook("displayBottom") ||
             !$this->registerHook("displayFrontFooter2") ||
@@ -123,7 +123,7 @@ class ItivosSocialsLink extends modules
                 }
             }
         }
-    	if (isIsset('submit_action')) {
+        if (isIsset('submit_action')) {
             unset($_POST['submit_action']);
             unset($_POST['submit']);
             foreach ($_POST as $key => $val) {
@@ -134,13 +134,13 @@ class ItivosSocialsLink extends modules
             $_SESSION['message'] = "Modulo actualizado correctamente";
             $_SESSION['type_message'] = "success";
             header("Location: ".__URI__.__ADMIN__."/modules/config/".$this->name."");
-    	}
+        }
         $te = itivosSocialsLinkC::getSocialsLinks();
         $values = array();
         foreach ($te as $key => $value) {
             $values[$value['id']] = $value['link'];
         }
-    	$helper = new HelperForm();
+        $helper = new HelperForm();
         $helper->tpl_vars = array(
             'fields_values' => $values,
             'languages' => language::getLangs(),
